843313050cc125769cdff0d96502969e23cf1ced
Matias Insaurralde <matias@insaurral.de>
2016-06-25 03:56:37 -0400

    Refactoring "errors"

--- goast/goast.go
+++ goast/goast.go
@@ -1,15 +1,16 @@
 package goast
 
 import "os"
+import "errors"
 import "strings"
 import "go/parser"
 import "go/token"
 // import "go/ast"
-import "goast/visitor"
-import "goast/walk"
-import "goast/tree"
+import "github.com/timtadh/GoAST/goast/visitor"
+import "github.com/timtadh/GoAST/goast/walk"
+import "github.com/timtadh/GoAST/goast/tree"
 
-func ParseFile(file_path string) (*tree.Node, os.Error) {
+func ParseFile(file_path string) (*tree.Node, error) {
     file, err := parser.ParseFile(token.NewFileSet(), file_path, nil, 0)
     if err != nil {
         return nil, err
@@ -22,7 +23,7 @@ func ParseFile(file_path string) (*tree.Node, os.Error) {
     return visitor.AST(), nil
 }
 
-func ParsePackage(dir_path, ext, package_name string) (*tree.Node, os.Error) {
+func ParsePackage(dir_path, ext, package_name string) (*tree.Node, error) {
     pkgs, err := ParseDirectory(dir_path, ext)
     if err != nil {
         return nil, err
@@ -30,10 +31,10 @@ func ParsePackage(dir_path, ext, package_name string) (*tree.Node, os.Error) {
     if pkg, has := pkgs[package_name]; has {
         return pkg, nil
     }
-    return nil, os.NewError("The supplied package name was not found.")
+    return nil, errors.New("The supplied package name was not found.")
 }
 
-func ParseDirectory(dir_path, ext string) (map[string]*tree.Node, os.Error) {
+func ParseDirectory(dir_path, ext string) (map[string]*tree.Node, error) {
     pkgs, err := parser.ParseDir(
         token.NewFileSet(),
         dir_path,
@@ -45,7 +46,7 @@ func ParseDirectory(dir_path, ext string) (map[string]*tree.Node, os.Error) {
         return nil, err
     }
     if len(pkgs) == 0 {
-        return nil, os.NewError("No packages found.")
+        return nil, errors.New("No packages found.")
     }
     pkgasts := make(map[string]*tree.Node)
     for name, node := range pkgs {
